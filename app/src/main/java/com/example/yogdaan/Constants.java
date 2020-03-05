8ab7f4b06ed8c5cf417c53adc265ce3eaac87e57
Anubhav <18ucs066@lnmiit.ac.in>
2020-03-05 21:57:17 +0530

    undo stuff

---
+++ app/src/main/java/com/example/yogdaan/Constants.java
@@ -0,0 +1,12 @@
+package com.example.yogdaan;
+
+import androidx.core.view.accessibility.AccessibilityViewCommand;
+
+public class Constants {
+    private static final String PACAKAGE_NAME="com.example.yogdaan";
+    static final String RESULT_DATA_KEY=PACAKAGE_NAME+".RESULT_DATA_KEY";
+    static final String RECEIVER=PACAKAGE_NAME+ ".RECEIVER";
+    static final String LOCATION_DATA_EXTRA=PACAKAGE_NAME +".LOCATION_DATA_EXTRA";
+    static final int SUCCESS_RESULT=1;
+    static final int FAILURE_RESULT=0;
+}
--- app/src/main/java/com/example/yogdaan/MainActivity.java
+++ app/src/main/java/com/example/yogdaan/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.yogdaan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.ResultReceiver;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
